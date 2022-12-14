f1e133467f800a437719a52d7b31a428bfef1630
ibrahimon <ibrahim07helal@gmail.com>
2022-12-14 12:16:21 +0000

    mapping

--- 0x04-python-more_data_structures/6-print_sorted_dictionary.py
+++ 0x04-python-more_data_structures/6-print_sorted_dictionary.py
@@ -1,3 +1,6 @@
 #!/usr/bin/python3
 def print_sorted_dictionary(a_dictionary):
-    s_dict = sorted(a_dictionary.items())
+    list_keys = list(a_dictionary.keys())
+    list_keys.sort()
+    for key in list_keys:
+        print("{}: {}".format(key, a_dictionary.get(key)))
