d7a02f445c4157e22fa155dfcc1229a19950ba8d
ibrahimon <ibrahim07helal@gmail.com>
2022-12-21 10:39:29 -0800

    exceptions

---
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+def safe_print_integer(value):
+    """Print an integer wit "{:d}".format().
+
+    Args:
+        value (int): The integer tp print.
+
+    Returns:
+        If a TypeError or ValueError occurs - False.
+        Otherwise - True
+    """
+
+    try:
+        print("{:d}".format(vlue))
+        return (True)
+    except (TypeError, ValueError):
+        return (False)
